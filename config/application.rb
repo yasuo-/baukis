759005b22f9415df85f3bcec1cc6b533efb9a30b
yasuo <breitling1216@gmail.com>
2018-03-14 12:58:23 +0900

    Add Setting Local & genetrators

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,15 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb, yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_spec false
+      g.view_spec false
+    end
   end
 end

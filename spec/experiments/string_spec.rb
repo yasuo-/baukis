3bd80b80774c1aa6962256ea58be92298994eb29
yasuo <breitling1216@gmail.com>
2018-03-16 09:10:22 +0900

    Add tag

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -8,10 +8,18 @@ describe String do
       expect(s.size).to eq(4)
     end
 
+    # 一時無効果
     xexample 'nilの追加' do
       s = "ABC"
       s << nil
       expect(s.size).to eq(4)
+    end
+
+    # Tagの追加 :exception
+    # command: rails spec --tag=exception
+    example 'nilを追加できない', :exception do
+      s = "ABC"
+      expect { s << nil }.to raise_error(TypeError)
     end
   end
 end

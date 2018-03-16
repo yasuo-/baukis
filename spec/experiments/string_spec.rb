d07833b8a75e090925799ee13798e4718c64d786
yasuo <breitling1216@gmail.com>
2018-03-16 09:03:23 +0900

    spec file

---
+++ spec/experiments/string_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe String do
+  describe '#<<' do
+    it '文字の追加' do
+      s = "ABC"
+      s << "D"
+      expect(s.size).to eq(4)
+    end
+
+    xexample 'nilの追加' do
+      s = "ABC"
+      s << nil
+      expect(s.size).to eq(4)
+    end
+  end
+end
